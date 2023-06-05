c0c985c087fa3e9a54035eff50a307f291167fd5
marcintubielewicz <marcin.tubielewicz@gmail.com>
2023-06-05 22:26:33 +0200

    code refactoring

--- src/main/scala/model/DAO/DatabaseConnection.scala
+++ src/main/scala/model/DAO/DatabaseConnection.scala
@@ -1,31 +1,5 @@
 package model.DAO
 
-import java.sql.{DriverManager, ResultSet}
+class DatabaseConnection {
 
-/**
- * Establishes a connection to a PostgreSQL database and retrieves data from the "weather_data" table.
- * The retrieved data is then printed to the console.
- */
-object DatabaseConnection extends App {
-
-  classOf[org.postgresql.Driver]
-  val con_str = "jdbc:postgresql://localhost:5432/weatherappdata?marcintubielewicz=DB_USER"
-  val conn = DriverManager.getConnection(con_str)
-  try {
-    val stm = conn.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY)
-
-    val rs = stm.executeQuery("SELECT * from weather_data")
-
-    while (rs.next) {
-      println("id: " + rs.getString("id") +
-        ", city_id: " + rs.getString("city_id") +
-        ", coord_lon: " + rs.getDouble("coord_lon") +
-        ", coord_lat: " + rs.getDouble("coord_lat") +
-        ", city_name: " + rs.getString("city_name")+
-        ",main_temp: " + rs.getDouble("main_temp"))
-      println()
-    }
-  } finally {
-    conn.close()
-  }
 }
